f87948bbcf818a958b3519aeaa7fe1d69b724cff
Diego Larrea <diego.larrea@konecta.com.py>
2020-04-26 07:41:28 -0400

    Estructura basica

--- personal-cv-front-v2/src/app/app.module.ts
+++ personal-cv-front-v2/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     AppRoutingModule
   ],
   providers: [],
